052631a7ae78ddd1f94a0e4c88b882a08aeb4b6e
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2022-07-10 10:20:05 -0300

    tentando resolver

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
@@ -9,6 +11,21 @@ import photoRoutes from './routes/photoRoutes';
 import './database';
 
 dotenv.config();
+
+const whiteList = [
+  'http://34.151.245.94',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, cb) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allod by CORS'));
+    }
+  },
+};
 class App {
   constructor() {
     this.app = express();
@@ -17,6 +34,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
